16ab99244a170ffebdb7eafd91d5a326d3d9d91d
DavidMorenoBautista <more5580@mylaurier.ca>
2019-03-26 00:44:15 -0400

    Finished sign up validation, began set up for database

---
+++ include/config.php
@@ -0,0 +1,23 @@
+<!-- config.php
+
+	CP476 Final Project
+
+	Author 1:  David Moreno-Bautista
+	ID 1:      151925580
+	Email 1:   more5580@mylaurier.ca
+
+	Author 2:  Xiang Ke
+	ID 2:	   150617130
+	Email 2:   kexx7130@mylaurier.ca
+
+	Version  2019-03-02
+	-->
+
+<?php
+
+define('DBHOST', 'localhost');
+define('DBUSER', 'root');
+define('DBPASS', '');
+define('DBNAME', 'APlusDB');
+
+?>
