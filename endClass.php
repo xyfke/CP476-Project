8a7bbfe22b5840034e7bdfe7f5723ba6f98e649c
DavidMorenoBautista <more5580@mylaurier.ca>
2019-03-30 15:32:23 -0400

    chat now displays when users join or leave the classroom

--- endClass.php
+++ endClass.php
@@ -15,6 +15,32 @@
 
 <?php
 	session_start();
+	include 'include/functions.php';
+	
+	$db = getDB();
+
+	date_default_timezone_set('America/New_York');
+	$timeRn = date("g:i A");
+	$query = "SELECT * FROM chat WHERE SessionID = ? AND UserID != ?";
+
+	if ($statement = mysqli_prepare($db, $query)) {
+		mysqli_stmt_bind_param($statement, 'ii', $_SESSION['classID'], $_SESSION['userId']);
+		mysqli_stmt_execute($statement);
+
+		$result = mysqli_stmt_get_result($statement);
+		$row = mysqli_fetch_array($result);
+		if($row){
+			$logName = $row[3];
+
+			$fp = fopen("chats/".$logName, 'a');
+			fwrite($fp,'<div class="container" style="text-align:center">'
+					   .'<div class="row pl-3" style="font-size:0.8em;font-weight:bold"><i>'.$_SESSION['userName'].' has left the chat</i></div>'
+					   .'<div class="row pl-3" style="font-size:0.8em"><i>'.$timeRn.'</i></div></div>'
+					   .'<div style="clear:both"></div>');
+			fclose($fp);
+		}
+	}
+
 	$sessStatHome = "sessZero";
 	$sessStatClass = "sessOne";
 	$_SESSION['sessStatHome'] = $sessStatHome;
