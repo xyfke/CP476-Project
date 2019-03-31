11e352d5251f3928e2a40e781e50f200e6164dda
root <kexx7130@mylaurier.ca>
2019-03-31 03:44:24 +0000

    edit .gitignore and canvas is working with mouse movements

---
+++ testAPI.php
@@ -0,0 +1,15 @@
+<html>
+    <header>
+        <title>White Board</title>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="./css/styles2.css">
+	    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <script type="text/javascript" src="./js/testAPI.js"></script>
+    </header>
+    <body>
+        <canvas class="drawSection" id="board"></canvas>
+        <div id="coordinates"></div>
+    </body>
+</html>
